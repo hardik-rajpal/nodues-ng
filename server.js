69d9c7cdcb64f52878b542e00585c2bc804e43e4
Hardik <hardikrajpal@cse.iitb.ac.in>
2022-03-21 23:54:45 +0530

    app-name fix

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/nodues-ng-2'));
+app.use(express.static('./dist/flexy-angular'));
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/nodues-ng-2/'}),
+    res.sendFile('index.html', {root: 'dist/flexy-angular/'}),
 );
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
